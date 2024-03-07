6332e70fd561861eef23e94d8a248a81aa320b62
sekishu <tobenaibutawa_tadanobuta21@yahoo.co.jp>
2024-03-07 10:54:17 +0900

    リモートリポジトリの変更

--- src/config/auth.php
+++ src/config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'atte_users',
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'atte_users',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'atte_users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Atte_user::class,
         ],
 
         // 'users' => [
@@ -87,8 +87,8 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
+        'atte_users' => [
+            'provider' => 'atte_users',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
